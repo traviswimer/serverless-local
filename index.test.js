641c1f0a0c547fec6a00036276fd64c30a8d14ea
Travis Wimer <notanactualemailaddress@traviswimer.com>
2018-04-26 20:34:53 -0500

    Adds plugin constructor unit tests.

--- index.test.js
+++ index.test.js
@@ -1,13 +1,60 @@
 const Serverless = require('serverless');
+const Promise = require('bluebird');
 
 const ServerlessLocalPlugin = require('./index');
 
-var plugin;
+var serverless;
 describe('ServerlessLocalPlugin', () => {
 	beforeEach(() => {
-		plugin = new ServerlessLocalPlugin();
+		serverless = {
+			cli: {
+				log: jest.fn(),
+				consoleLog: jest.fn()
+			},
+			pluginManager: {
+				spawnzz: jest.fn()
+			},
+			service: {
+				resources: {}
+			},
+			getProvider: jest.fn(() => {
+				return {
+					sdk: {
+						config: {
+							update: jest.fn(),
+							setPromisesDependency: jest.fn()
+						}
+					}
+				}
+			})
+		}
 	});
 
-	test('', () => {
+	describe('constructor()', () => {
+		test('default config is used if no user config provided', () => {
+			let plugin = new ServerlessLocalPlugin(serverless);
+			expect(plugin.config).toMatchSnapshot();
+		});
+
+		test('user config will override default', () => {
+			serverless.service.custom = {
+				'serverless-local': {
+					host: 'http://otherhost',
+					region: 'elsewhere',
+					ports: {
+						"dynamodb": 1111,
+						"redshift": 2222,
+						"cloudformation": 3333,
+						"sqs": 4444,
+					},
+					endpoints: {
+						dynamodbstreams: 'https://randomhost:1234',
+						route53: 'http://randomhost:4321'
+					}
+				}
+			}
+			let plugin = new ServerlessLocalPlugin(serverless);
+			expect(plugin.config).toMatchSnapshot();
+		});
 	});
 });
